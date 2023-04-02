513236bb93dbdf29ff896ad5452ba106fe1ecebc
Oleksandr Pronin <pronin.alx@gmail.com>
2023-04-02 08:49:31 +0200

    Initial commit

---
+++ Payback/PaybackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaybackApp.swift
+//  Payback
+//
+//  Created by Pronin Oleksandr on 02.04.23.
+//
+
+import SwiftUI
+
+@main
+struct PaybackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
